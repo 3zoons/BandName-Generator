0d67c628884f9a7ac50ec3e1ca457c0e7894ac2a
3zoons <aahhkns@gmail.com>
2024-02-09 04:26:49 +0530

    initial

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Welcome to Band Name Generator.")
+
+cityName = input("What is the name of the city you grew up in?\n")
+petName = input("What is your pet's name?\n")
+
+print(f"Your band name could be {cityName} {petName}.")
